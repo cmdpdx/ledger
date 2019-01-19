9bd62ce4764e56789e4d7c1f25e752b15a6de7e0
Colin Douglas <16968564+cmdpdx@users.noreply.github.com>
2019-01-18 23:30:47 -0800

    moved CLI into separate file
    Moved the CLI class into a separate file to logically separate the accounts/management from the interaction.

--- ledger.cs
+++ ledger.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace Ledger {
     class Account {
@@ -141,18 +141,16 @@ namespace Ledger {
     }
 
 
-    // TODO: add reference to currentAccount, rather than coding the pathways through.
-    class AccountManager {
+    public class AccountManager {
         private Account currentAccount;
         private List<Account> accounts;
 
-        // constructor
         public AccountManager() {
             accounts = new List<Account>();
             currentAccount = null;
         }
 
-        // properties
+        // Public properties; all read-only
         public bool LoggedOn {
             get { return currentAccount != null; }
         }
@@ -166,9 +164,14 @@ namespace Ledger {
             get { return LoggedOn ? currentAccount.TransactionHistory : null; }
         }
 
-        // public methods
+        // Create a new user account with the supplied credentials.
+        // Parameters:
+        //  string user/pass - username and password, respectively, of account to create.
+        //  string msg - Output variable; message indicates success or failure (username already exists)
+        // Returns: 
+        //  bool - true if account successfully created; false otherwise
         public bool CreateAccount(string user, string pass, out string msg) {
-            // does this username already exist?
+            // Does this username already exist?
             if (isValidUser(user)) {
                 msg = $"Username {user} already exists.";
                 return false;
@@ -179,30 +182,43 @@ namespace Ledger {
             return true;
         }
 
+        // Attempt to log onto on account with the supplied credentials.
+        // Parameters:
+        //  string user/pass - username and password, respectively, of account.
+        //  string msg - Output variable; error message on failure, timestamp of last logon on success
+        // Returns: 
+        //  bool - true if successfully logged on; false otherwise
         public bool LogOn(string user, string pass, out string msg) {
-            // check that the user actually exists
+            // Check that the user actually exists.
             if (!isValidUser(user)) {
                 msg = $"Username {user} not recognized. Create new account.";
                 return false;
             }
-            // find the correct account, check the password supplied
+            // Find the correct account, check the password supplied matches.
             var account = accounts.Find(x => x.UserName.Equals(user));
             if (!account.CheckPassword(pass)) {
                 msg = "Invalid password.";
                 return false;
             }
-            
+            // Set the currentAccount to the authenticated account and update last log on time
             currentAccount = account;
             msg = currentAccount.LastLogOn.ToString();
             currentAccount.UpdateLastLogOn();
             return true;
         }
 
+        // Log out of the current account.
         public void LogOut() {
             //currentAccount.store();
             currentAccount = null;
         }
 
+        // Attempt to deposit into the current account, if logged on.
+        // Parameters:
+        //  double amount - Value to deposit into the current account.
+        //  string msg - Output variable; error message on failure; confirmation on success
+        // Returns:
+        //  bool - true if successfully deposited; false otherwise.
         public bool Deposit(double amount, out string msg) {
             if (!LoggedOn) {
                 msg = "Not logged in.";
@@ -211,6 +227,12 @@ namespace Ledger {
             return currentAccount.Deposit(amount, out msg);
         }
 
+        // Attempt to withdraw from the current account, if logged on.
+        // Parameters:
+        //  double amount - Value to withdraw from the current account.
+        //  string msg - Output variable; error message on failure; confirmation on success
+        // Returns:
+        //  bool - true if successfully withdrawn; false otherwise.
         public bool Withdraw(double amount, out string msg) {
             if (!LoggedOn) {
                 msg = "Not logged in.";
@@ -219,154 +241,14 @@ namespace Ledger {
             return currentAccount.Withdraw(amount, out msg);
         }
 
-        // private methods
+        // Private method: checks whether a given name matches in the list of 
+        // current users known. Returns true if user is found, false otherwise.
         private bool isValidUser(string user) {
             foreach (var account in accounts) {
                 if (account.UserName.Equals(user))
                     return true;
             }
             return false;
-        }
-    }
-
-    // TODO: If user is not logged in, only give options to log on or add new user.
-    class CLI {
-        private AccountManager manager;
-        private Dictionary<string, Action> options;
-
-        public CLI() {
-            manager = new AccountManager();
-            options = new Dictionary<string, Action>();
-            options["new"] = newUser;
-            options["logon"] = logOnUser;
-            options["logout"] = logOutUser;
-            options["balance"] = balance;
-            options["deposit"] = deposit;
-            options["withdraw"] = withdraw;
-            options["history"] = history;
-            options["help"] = help;
-
-        }
-
-        public void Run() {
-            string input = string.Empty;
-            Console.WriteLine("Transaction Ledger.");
-            Console.WriteLine("Create new account with \"new\". Type \"help\" to see all options.");
-            while (true) {
-                Console.Write($"{manager.LoggedOn ? manager.UserName : ""}>");
-                input = Console.ReadLine();
-
-                // Ensure user cannot perform actions that require login without being logged on.
-                // ...feels like there is a better way, but I'll go with this for now...
-                if (!manager.LoggedOn && (input.Equals("balance") || input.Equals("deposit") || input.Equals("withdraw") || input.Equals("history"))) {
-                    Console.WriteLine($"Must be logged on to perform \"{input}\".");
-                } else if (input.Equals("quit")) {
-                    break;
-                } else if (!options.ContainsKey(input)) {
-                    Console.WriteLine($"Option \"{input}\" not recognized."); 
-                } else {
-                    options[input]();
-                }
-                
-                Console.WriteLine();
-            } 
-        }
-
-        // TODO: load existing users from persistant storage
-        private void loadUsers() {
-
-        }
-
-        private void help() {
-            Console.WriteLine("Always available: new, logon, logout, help, quit");
-            Console.WriteLine("When logged on: balance, deposit, withdraw, history");
-        }
-        
-        private void newUser() {
-            if (manager.LoggedOn) {
-                Console.Write($"Already logged in as user {manager.UserName}, logout (y/n)? ");
-                string input = Console.ReadLine();
-                if (!input.Equals("y"))
-                    return;
-                logOutUser();
-            }
-
-            Console.Write("Choose username: ");
-            string user = Console.ReadLine();
-            Console.Write("Choose password: ");
-            string pass = Console.ReadLine();
-            
-            bool success = manager.CreateAccount(user, pass, out string msg);
-            if (!success) 
-                Console.WriteLine($"User creation failed: {msg}");
-        }
-
-        private void logOnUser() {
-            if (manager.LoggedOn) {
-                Console.Write($"Already logged on as user {manager.UserName}, logout (y/n)? ");
-                string input = Console.ReadLine();
-                if (!input.Equals("y"))
-                    return;
-                logOutUser();
-            }
-
-            Console.Write("username: ");
-            string user = Console.ReadLine();
-            Console.Write("password: ");
-            string pass = Console.ReadLine();
-
-            bool success = manager.LogOn(user, pass, out string msg);
-            if (!success)
-                Console.WriteLine($"Log on failed: {msg}");
-            else
-                Console.WriteLine($"User {user} logged on. Last Log on at {msg}");
-        }
-
-        private void logOutUser() {
-            if (!manager.LoggedOn) 
-                return;
-            
-            manager.LogOut();
-        }
-
-        private void balance() {
-            Console.WriteLine($"Current balance: ${manager.Balance}");
-        }
-
-        private void deposit() {
-            Console.Write("Despoit amount > ");
-            string input = Console.ReadLine();
-            double amount;
-            if (!Double.TryParse(input, out amount)) {
-                Console.WriteLine($"Invalid deposit input: {input}.");
-                return;
-            }
-
-            bool success = manager.Deposit(amount, out string msg);
-            if (!success)
-                Console.WriteLine($"Deposit failed: {msg}");            
-        }
-
-        private void withdraw() {
-            Console.Write("Withdrawl amount > ");
-            string input = Console.ReadLine();
-            double amount;
-            if (!Double.TryParse(input, out amount)) {
-                Console.WriteLine($"Invalid withdrawl input: {input}.");
-                return;
-            }
-
-            bool success = manager.Withdraw(amount, out string msg);
-            if (!success) 
-                Console.WriteLine($"Withdrawl failed: {msg}");    
-        }
-
-        private void history() {
-            Console.WriteLine($"Transaction history for user {manager.UserName}:");
-            var transactions = manager.TransactionHistory;
-            foreach (var t in transactions) {
-                Console.WriteLine(t);
-            }
         }
     }
 }
